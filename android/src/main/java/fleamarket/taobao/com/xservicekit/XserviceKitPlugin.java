d04e55fa5994dcfce5100d5bd54f47f6ed88f9dc
maxu <maxu@100tal.com>
2020-06-11 15:00:31 +0800

    兼容v2插件

--- android/src/main/java/fleamarket/taobao/com/xservicekit/XserviceKitPlugin.java
+++ android/src/main/java/fleamarket/taobao/com/xservicekit/XserviceKitPlugin.java
@@ -1,30 +1,38 @@
 package fleamarket.taobao.com.xservicekit;
 
+import androidx.annotation.NonNull;
+
 import fleamarket.taobao.com.xservicekit.message.MessengerFacade;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/** XserviceKitPlugin */
-public class XserviceKitPlugin implements MethodCallHandler {
-  /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "xservice_kit");
-    channel.setMethodCallHandler(new XserviceKitPlugin());
-
-    //Connect to messenger.
-    MessengerFacade.sharedInstance().setMessenger(registrar.messenger());
-
-  }
-
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
+/**
+ * XserviceKitPlugin
+ */
+public class XserviceKitPlugin implements FlutterPlugin, MethodCallHandler {
+    @Override
+    public void onMethodCall(MethodCall call, Result result) {
+        if (call.method.equals("getPlatformVersion")) {
+            result.success("Android " + android.os.Build.VERSION.RELEASE);
+        } else {
+            result.notImplemented();
+        }
     }
-  }
+
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+        final MethodChannel channel = new MethodChannel(binding.getBinaryMessenger(), "xservice_kit");
+        channel.setMethodCallHandler(this);
+
+        //Connect to messenger.
+        MessengerFacade.sharedInstance().setMessenger(binding.getBinaryMessenger());
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+
+    }
 }
